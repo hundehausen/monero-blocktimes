231c847df6030f8054f3af9e7c1b65019b5b4148
hundehausen <mail@grischadaum.de>
2020-07-09 00:28:50 +0200

    blocktime_calc.js angepasst

--- blocktime_calc.js
+++ blocktime_calc.js
@@ -1,26 +1,11 @@
 const fetch = require("node-fetch");
-
-/*
-    Dieses Script holt sich von einem Monero Node mit aktiviertem JSON RPC
-    die letzten n Blöcke und berechnet deren Zeitdifferenz.
-    Der Timestamp wird in jedem Block vom Miner selbst festgelegt. Hierbei
-    kommt es zu Abweichungen, da die Miner keine gemeinsame synchronisierte
-    Systemzeit nutzen.
-    Das Monero Netzwerk lehnt den Block ab, wenn der Timestamp größer als
-    die Median Blockdauer der letzten 60 Blöcke ist.
-    siehe: https://github.com/monero-project/monero/blob/master/src/cryptonote_core/blockchain.cpp#L1691
-    und https://github.com/monero-project/monero/blob/master/src/cryptonote_core/blockchain.cpp#L3611
-    Aufgrundessen kommt es unvermeidlich zu unkorrekten Ergebnissen bei diesem
-    Programm, allerdings kann die Abweichung nicht größer als die Median Blockzeit
-    der letzten 60 Blöcke sein. Die Werte dienen somit nur als Richtwerte.
-
-    Autor: Grischa Daum
-*/
+const ObjectsToCsv = require("objects-to-csv");
 
 // Monero Mainnet RPC
 let url = "http://192.168.0.119:18081/json_rpc";
+
 // Anzahl der letzten Blöcke, die untersucht werden sollen
-const LAST_N_BLOCKS = 1000;
+const LAST_N_BLOCKS = 100;
 
 async function getBlockheight() {
   let data = {
@@ -35,6 +20,7 @@ async function getBlockheight() {
     body: JSON.stringify(data),
   });
   let response_json = await response.json();
+  console.log("Blockheight:", response_json.result.block_header.height);
   return await response_json.result.block_header.height;
 }
 
@@ -44,7 +30,7 @@ async function getBlockHeaders(start, end) {
     jsonrpc: "2.0",
     method: "get_block_headers_range",
     params: {
-      start_height: start,
+      start_height: start - 1,
       end_height: end,
     },
   };
@@ -60,59 +46,87 @@ async function getBlockHeaders(start, end) {
 }
 
 async function getTimestamps(blockheaders) {
-  let timestamps = [];
-  blockheaders.map((x, index, arr) => {
-    timestamps.push(x.timestamp);
+  return blockheaders.map(({ height, timestamp }) => {
+    return { height: height, timestamp: timestamp };
   });
-  return timestamps; // Array
 }
 
 async function getTimedeltas(timestamps) {
   let timedeltas = [];
   timestamps.map((x, index, arr) => {
-    // Zeitdiffrenzen zwischen allen Blöcken n berechnen (n-1)
+    // Calculate time difference of selected n blocks - (n-1) results
     if (index != arr.length - 1) {
-      timedeltas.push(arr[index + 1] - x);
-    }
-  });
-  return timedeltas; // Array
-}
-
-// Asynchrone Funktionsaufrufe
-getBlockheight().then(async (height) => {
-  start_height = height - LAST_N_BLOCKS;
-  console.log("First block:", start_height, "Last block:", height);
-
-  let blockheaders = getBlockHeaders(start_height, height).then(
-    async (blockheaders) => {
-      getTimestamps(blockheaders).then(async (timestamps) => {
-        getTimedeltas(timestamps).then(async (timedeltas) => {
-          let min = Math.min(...timedeltas); // kuerzeste Blockzeit
-          let max = Math.max(...timedeltas); // laengste Blockzeit
-          let sum = 0;
-          timedeltas.forEach((delta) => {
-            sum += delta;
-          });
-          let average = sum / timedeltas.length;
-          console.log(
-            "Average blocktime of last",
-            height - start_height,
-            "blocks:",
-            Math.round((average + Number.EPSILON) * 100) / 100,
-            "seconds or",
-            Math.round((average / 60.0 + Number.EPSILON) * 100) / 100,
-            "minutes"
-          );
-          console.log("Minimum time:", min, "seconds");
-          console.log(
-            "Maximum time:",
-            max,
-            "seconds or",
-            Math.round((max / 60.0 + Number.EPSILON) * 100) / 100,
-            "minutes"
-          );
-        });
+      timedeltas.push({
+        height: x.height + 1,
+        blocktime: arr[index + 1].timestamp - x.timestamp,
       });
     }
+  });
+  return timedeltas;
+}
+
+async function calcDeviation(blocktimes) {
+  blocktimes.map((block) => {
+    // deviation from the target value of 120 seconds per block
+    block.deviation = Math.abs(120 - block.blocktime);
+  });
+  return blocktimes;
+}
+
+async function calcStats(blocktimes) {
+  let min = Math.min.apply(
+    Math,
+    blocktimes.map((block) => {
+      return block.blocktime;
+    })
+  ); // shortest blocktime - can be negative
+  let max = Math.max.apply(
+    Math,
+    blocktimes.map((block) => {
+      return block.blocktime;
+    })
+  ); // longest blocktime
+  let sum = 0;
+  blocktimes.forEach((block) => {
+    sum += block.blocktime;
+  });
+  let average = sum / blocktimes.length;
+  console.log(
+    "First block:",
+    blocktimes[0].height,
+    "Last block:",
+    blocktimes[blocktimes.length - 1].height
   );
-});
+  console.log(
+    "Average blocktime of last",
+    blocktimes.length,
+    "blocks:",
+    Math.round((average + Number.EPSILON) * 100) / 100,
+    "seconds or",
+    Math.round((average / 60.0 + Number.EPSILON) * 100) / 100,
+    "minutes"
+  );
+  console.log("Minimum time:", min, "seconds");
+  console.log(
+    "Maximum time:",
+    max,
+    "seconds or",
+    Math.round((max / 60.0 + Number.EPSILON) * 100) / 100,
+    "minutes"
+  );
+  return blocktimes;
+}
+
+async function exportCsv(blocktimes) {
+  const csv = new ObjectsToCsv(blocktimes);
+  // Save to file:
+  await csv.toDisk("./blocktimes.csv");
+}
+
+getBlockheight()
+  .then((height) => getBlockHeaders(height - LAST_N_BLOCKS, height))
+  .then(getTimestamps)
+  .then(getTimedeltas)
+  .then(calcDeviation)
+  .then(calcStats)
+  .then(exportCsv);
