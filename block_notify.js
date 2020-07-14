e876b7620b40c613043f54f286d7be2694bffeaa
hundehausen <mail@grischadaum.de>
2020-07-14 22:01:47 +0200

    added block_noitfy.js script for ./monerod --block-notify

---
+++ block_notify.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+let currentTime = Math.round(new Date().getTime() / 1000);
+
+// Monero Mainnet RPC
+let url = "http://192.168.0.119:18081/json_rpc";
+
+async function getBlockheight() {
+  let data = {
+    jsonrpc: "2.0",
+    method: "get_last_block_header",
+  };
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  let response_json = await response.json();
+  return await response_json.result.block_header.height;
+}
+
+async function appedToCsv(blockheight, time) {
+  let newBlock = String((await blockheight) + "," + time + "\n");
+  fs.appendFile("blocklogger.csv", newBlock, function (err) {
+    if (err) throw err;
+  });
+}
+
+appedToCsv(getBlockheight(), currentTime);
