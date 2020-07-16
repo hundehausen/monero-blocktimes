efdbd996ce9d35e3f205f7bcbf5e126a2e437efa
hundehausen <mail@grischadaum.de>
2020-07-16 16:26:41 +0200

    new function getBlockHeaderByHash

--- block_notify.js
+++ block_notify.js
@@ -6,10 +6,26 @@ let currentTime = Math.round(new Date().getTime() / 1000);
 // Monero Mainnet RPC
 let url = "http://192.168.0.119:18081/json_rpc";
 
-async function getBlockheight() {
+async function getLastBlockHeader() {
   let data = {
     jsonrpc: "2.0",
     method: "get_last_block_header",
+  };
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  let response_json = await response.json();
+  return await response_json.result.block_header.height;
+}
+
+async function getBlockHeaderByHash(hash) {
+  let data = {
+    jsonrpc: "2.0",
+    method: "get_block_header_by_hash",
   };
   let response = await fetch(url, {
     method: "POST",
@@ -29,4 +45,4 @@ async function appedToCsv(blockheight, time) {
   });
 }
 
-appedToCsv(getBlockheight(), currentTime);
+appedToCsv(getBlockHeaderByHash(process.argv[2]), currentTime);
